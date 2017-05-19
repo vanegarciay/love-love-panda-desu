44d714b22f2df82ed1b4c2276b750a9793dc5d0a
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-19 02:11:07 -0400

    trabajo final completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+pictures.forEach(function(imagen){
+    var imagenes = document.getElementById("imagenes");
+    var img = document.createElement("img");
+    img.setAttribute("src", imagen);
+    img.classList.add("panditas");
+    imagenes.appendChild(img);
+});
